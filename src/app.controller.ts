bebf1cec29274f4a4b2374072dce7007c2e4ff61
Joilson Leal <joilson_rg@hotmail.com>
2021-08-21 17:53:00 -0300

    feat: add authorization/authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,13 @@
-import { UseGuards } from '@nestjs/common';
-import { Controller, Get, Post, Request } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './server/auth/auth.service';
-import { JwtAuthGuard } from './server/auth/jwt-auth.guard';
-import { LocalAuthGuard } from './server/auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('protected')
-  getHello(): string {
-    return 'hello';
+  async login(@Body() user: any) {
+    return this.authService.login(user);
   }
 }
--- src/server/auth/auth.service.ts
+++ src/server/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { AdminService } from '../admin/admin.service';
@@ -10,19 +10,16 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.adminService.findByEmail(email);
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const { password, ...result } = user;
-      return result;
+  async login(data: any) {
+    const user = await this.adminService.findByEmail(data.email);
+    if (user && bcrypt.compareSync(data.password, user.password)) {
+      const payload = { username: user.name, sub: user.id };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
     }
-    return null;
-  }
-
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+    return new UnauthorizedException({
+      message: 'E-mail or password incorrect',
+    });
   }
 }
